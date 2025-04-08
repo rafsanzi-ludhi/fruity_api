3beb31ed6b131c29da6bc627c9cc6a3794780156
Rafsanzani <rafsanzaniludhi@gmail.com>
2025-04-08 11:08:27 +0100

    added in cors

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.get("/", (req, res) => {
   res.send("Hello fruity!")
 })
 
-app.use(cors())
+
+app.use(cors()) // Middleware to enable CORS
 app.use(express.json()) // Middleware to parse JSON request bodies
 app.use(`/fruits`, fruits)
 
